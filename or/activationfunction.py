55bcf86e2f23bcde8c8a1e77a440aee04732b50a
kaikai529 <45989030+kaikai529@users.noreply.github.com>
2020-10-26 22:12:13 +0900

    #4 func update (#6)
    * add weight module(vixier and he)
    
    * add tanh,ReLU
    
    * delete kaikai specific settings
    
    Co-authored-by: Hattomo <45988993+Hattomo@users.noreply.github.com>

--- or/activationfunction.py
+++ or/activationfunction.py
@@ -13,16 +13,63 @@ def msigmoid(x):
     for i in range(1,node):
         ans[i] = sigmoid(x[i])
         return ans
-
+# tanh関数
+def tanh(x):
+    e = math.e
+    return (e**x-e**-x)/(e**x+e**-x)
+# mtanh(一次元配列　x)
+def mtanh(x):
+    ans = x
+    node = len(x)
+    for i in range(1,node):
+        ans[i] = tanh(x[i])
+        return ans
+# ReLU関数
+def ReLU(x):
+    if x<0:
+        return 0
+    else:
+        return x
+# mReLU(一次元配列　x)
+def mReLU(x):
+    ans = x
+    node = len(x)
+    for i in range(1,node):
+        ans[i] = ReLU(x[i])
+        return ans
 ## 活性化関数の微分
 
 # sigmoid関数の微分
 def diffsigmoid(x):
     return sigmoid(x)*(1-sigmoid(x))
-# diffsigmoid(一次元配列 x)
+# mdiffsigmoid(一次元配列 x)
 def mdiffsigmoid(x):
     ans = x
     node = len(x)
     for i in range(node):
         ans[i] = diffsigmoid(x[i])
+    return ans
+# tanh関数の微分
+def difftanh(x):
+    e = math.e
+    return 4/(e**x+e**-x)**2
+# mdifftanh(一次元配列 x)
+def mdifftanh(x):
+    ans = x
+    node = len(x)
+    for i in range(node):
+        ans[i] = difftanh(x[i])
+    return ans
+# ReLU関数の微分
+def diffReLU(x):
+    if x<0:
+        return 0
+    else:
+        return 1
+# mdiffReLU(一次元配列 x)
+def mdiffReLU(x):
+    ans = x
+    node = len(x)
+    for i in range(node):
+        ans[i] = diffReLU(x[i])
     return ans
