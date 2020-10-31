4614d86656540bccc6d2b420e040c48910421968
Hattomo (TomohiroHattori) <45988993+Hattomo@users.noreply.github.com>
2020-10-31 20:59:46 +0900

    #13 analysis (#16)
    * Happy Halloween
    
    * Add 3D chart
    
    * nits

--- dnn/main.py
+++ dnn/main.py
@@ -1,19 +1,22 @@
 import activationfunction as af
 import makedata as md
 import neural_network as nn
+import analysistool as atool
 
 layer = [3, 3, 1]
-epoch = 25
-
+epoch = 30
+logic = "xor"
 # set data
-trainData = md.dset(epoch)
-testData = md.dset(10)
+trainData = md.dset(logic,epoch)
+testData = md.dset(logic,20)
 # ニューラルネットワークの生成
 orNN = nn.Neural_Network(layer)
 # 学習
 count = 200
 # ニューラルネットワークのトレーニングデータ、レイヤー、重み番号、活性化関数番号の設定,cost func
-orNN.model(trainData,testData,"xivier","sigmoid","rss")
+orNN.model(trainData, testData, "xivier", "sigmoid", "rss")
 for i in range(count):
     orNN.train()
     orNN.test()
+atool.draw(orNN.cost)
+atool.tdchart(orNN)
