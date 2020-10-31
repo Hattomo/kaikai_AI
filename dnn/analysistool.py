610db634317d959d331756eb20c9fab94b7226ac
Hattomo <45988993+Hattomo@users.noreply.github.com>
2020-10-31 16:39:14 +0900

    Add 3D chart

--- dnn/analysistool.py
+++ dnn/analysistool.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import neural_network as nn
+from mpl_toolkits.mplot3d import Axes3D
 
 def draw(y):
     fig = plt.figure()
@@ -10,7 +11,22 @@ def draw(y):
     #plt.show()
     plt.savefig("dnn/out/cost.png")
 
-def chart():
-    for i in range(10):
-        for j in range(10):
-            pass
+def tdchart(nn):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.set_xlabel('x0')
+    ax.set_ylabel('x1')
+    ax.set_zlabel('y')
+    x = []
+    y = []
+    z = []
+    dence = 30
+    for i in range(dence):
+        for j in range(dence):
+            x.append(i/dence)
+            y.append(j / dence)
+            nn.forwordpropagation([i/dence,j/dence])
+            z.append(nn.z[-1])
+    ax.scatter3D(x, y, z)
+    plt.show()
+    plt.savefig("dnn/out/3d.png")   
--- dnn/main.py
+++ dnn/main.py
@@ -3,19 +3,20 @@ import makedata as md
 import neural_network as nn
 import analysistool as atool
 
-layer = [3, 4, 4, 1]
+layer = [3, 3, 1]
 epoch = 30
-
+logic = "xor"
 # set data
-trainData = md.dset("xor",epoch)
-testData = md.dset("xor",20)
+trainData = md.dset(logic,epoch)
+testData = md.dset(logic,20)
 # ニューラルネットワークの生成
 orNN = nn.Neural_Network(layer)
 # 学習
-count = 1000
+count = 200
 # ニューラルネットワークのトレーニングデータ、レイヤー、重み番号、活性化関数番号の設定,cost func
 orNN.model(trainData, testData, "xivier", "sigmoid", "rss")
 for i in range(count):
     orNN.train()
     orNN.test()
 atool.draw(orNN.cost)
+atool.tdchart(orNN)
