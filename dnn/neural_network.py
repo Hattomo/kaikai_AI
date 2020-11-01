67eb3db6a5876200b11d52cf7a11b52ff9b4caad
Hattomo (TomohiroHattori) <45988993+Hattomo@users.noreply.github.com>
2020-11-01 15:09:16 +0900

    Formatter (#20)
    Format

--- dnn/neural_network.py
+++ dnn/neural_network.py
@@ -6,7 +6,8 @@ import numpy as np
 import vectormath as vmath
 
 class Neural_Network:
-    def __init__(self,layer):
+
+    def __init__(self, layer):
         self.layer = layer
         self.length = len(layer)
         self.y = None
@@ -19,18 +20,18 @@ class Neural_Network:
         self.train_ratio = 0.5
         self.cost = list()
 
-    def model(self,data,testdata,w_method="unif",actfunc="sigmoid",costfunc="rss"):
+    def model(self, data, testdata, w_method="unif", actfunc="sigmoid", costfunc="rss"):
         self.data = data
         self.testdata = testdata
         self.y = setting.ynet(self.layer)
         self.z = setting.znet(self.layer)
         # 重みの初期化
         if w_method == "xivier":
-            self.weight = setting.wnet(self.layer,setting.xivier)
+            self.weight = setting.wnet(self.layer, setting.xivier)
         elif w_method == "he":
-            self.weight = setting.wnet(self.layer,setting.he)
+            self.weight = setting.wnet(self.layer, setting.he)
         elif w_method == "unif":
-            self.weight = setting.wnet(self.layer,setting.unif)
+            self.weight = setting.wnet(self.layer, setting.unif)
         else:
             sys.stdout.write("Error: The weight method is not found\n")
             sys.exit(1)
@@ -40,7 +41,7 @@ class Neural_Network:
             self.difffunc = af.diffsigmoid
         elif actfunc == "tanh":
             self.actfunc = af.tanh
-            self.difffunc= af.difftanh
+            self.difffunc = af.difftanh
         elif actfunc == "ReLU":
             self.actfunc = af.ReLU
             self.difffunc = af.diffReLU
@@ -56,35 +57,35 @@ class Neural_Network:
         self.difffunc = af.msigmoid
 
     # フォワードプロパゲーション
-    def forwordpropagation(self,x):
+    def forwordpropagation(self, x):
         self.z[0][0] = 1
         self.z[0][1:] = x
         for i in range(len(self.layer) - 1):
             if i == len(self.weight) - 1:
-                self.y[i+1] = self.weight[i] @ self.z[i]
-                self.z[i+1] = self.actfunc(self.y[i+1])
+                self.y[i + 1] = self.weight[i] @ self.z[i]
+                self.z[i + 1] = self.actfunc(self.y[i + 1])
                 break
-            self.y[i+1][1:] = self.weight[i] @ self.z[i]
+            self.y[i + 1][1:] = self.weight[i] @ self.z[i]
             self.z[i + 1] = self.actfunc(self.y[i + 1])
-            
+
     # バックプロパゲーション
     def backpropagation(self, x, y):
         # out layer to middle layer
-        if (self.z[-1] - y)** 2 < 0.5:
+        if (self.z[-1] - y)**2 < 0.5:
             self.train_ratio = 0.1
-        elif (self.z[-1] - y)** 2 < 0.1:
+        elif (self.z[-1] - y)**2 < 0.1:
             self.train_ratio = 0.01
-        tmp = self.difffunc(self.y[-1]) * (self.z[-1]-y)
+        tmp = self.difffunc(self.y[-1]) * (self.z[-1] - y)
         diff = self.z[-2] * tmp
         self.weight[-1] -= self.train_ratio * diff
         # middle layer to input layer
         for i in range(len(self.layer) - 2):
-            weight = (self.weight[-i-1].T[1:]).T
-            z = (self.z[-i-2].T[1:]).T
+            weight = (self.weight[-i - 1].T[1:]).T
+            z = (self.z[-i - 2].T[1:]).T
             tmp = self.difffunc(z) * (weight.T @ tmp)
-            diff = vmath.vvmat(self.z[-i-3],tmp)
-            self.weight[-i-2] -= self.train_ratio * diff.T
-            
+            diff = vmath.vvmat(self.z[-i - 3], tmp)
+            self.weight[-i - 2] -= self.train_ratio * diff.T
+
     # 学習
     def train(self):
         length = len(self.data)
@@ -103,6 +104,8 @@ class Neural_Network:
                 count += 1
             elif self.z[-1] <= 0.2 and self.testdata[i][-1] == 0:
                 count += 1
-            cost += ((self.z[-1] - self.testdata[i][-1])** 2)
-        self.cost.append(cost/length)
-        print(str(count) + "/" + str(length) + " = " + str(count / length) + " : " + str(cost) + " : " + str(len(self.cost)))
+            cost += ((self.z[-1] - self.testdata[i][-1])**2)
+        self.cost.append(cost / length)
+        print(
+            str(count) + "/" + str(length) + " = " + str(count / length) + " : " + str(cost) + " : " +
+            str(len(self.cost)))
