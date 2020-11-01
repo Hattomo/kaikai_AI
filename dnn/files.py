dc24dfa9f3d0a32de3ab859dc78012a3b5103413
Hattomo <45988993+Hattomo@users.noreply.github.com>
2020-11-01 15:14:01 +0900

    format

--- dnn/files.py
+++ dnn/files.py
@@ -2,15 +2,12 @@ import sys
 import os
 import numpy as np
 
-
 # save wieght
 def save(nn):
     np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
     path = os.path.join(os.path.dirname(__file__), 'out/np_savez')
     np.savez(path, weight=nn.weight)
-    np.warnings.filterwarnings('default',
-                               category=np.VisibleDeprecationWarning)
-
+    np.warnings.filterwarnings('default', category=np.VisibleDeprecationWarning)
 
 # load weight
 def load(path):
