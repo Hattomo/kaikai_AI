f8ddd92e0a86ceeafffb1f20324017e08238822a
Hattomo (TomohiroHattori) <45988993+Hattomo@users.noreply.github.com>
2020-11-04 05:35:39 +0900

    Add MNIST dataset for nn (#46)
    * Add MNIST dataset for nn
    
    (cherry picked from commit 5806515d007c490df3dd55bda60f64d98f4de63a)
    
    * fix var name

--- dataset/logic_circuit.py
+++ dataset/logic_circuit.py
@@ -3,6 +3,9 @@ import random
 
 import numpy as np
 
+sys.path.append('./dataset')
+import mnist
+
 # データの作成
 # num データの数
 # data 0,1番目が学習データ 2番目が答え
@@ -46,6 +49,28 @@ def dset(d_name, num):
             data[4*i + 1] = [0, 1, 1, 0]
             data[4*i + 2] = [1, 0, 0, 1]
             data[4*i + 3] = [1, 1, 0, 0]
+        return data
+    elif d_name == "mnist_train" or d_name == "mnist_test":
+        (train_data, train_label), (test_data, test_label) = mnist.load_data()
+        data = np.zeros((num, 784 + 10), dtype=np.float128)
+        for i in range(num):
+            for j in range(28):
+                for k in range(28):
+                    if (d_name == "mnist_train"):
+                        data[i][28*j + k] = train_data[i][j][k]
+                    elif (d_name == "mnist_test"):
+                        data[i][28*j + k] = test_data[i][j][k]
+            for l in range(784, 794):
+                if (d_name == "mnist_train"):
+                    if (l - 784 == train_label[i]):
+                        data[i][l] = 1.0
+                    else:
+                        data[i][l] = 0.0
+                elif (d_name == "mnist_test"):
+                    if (l - 784 == train_label[i]):
+                        data[i][l] = 1.0
+                    else:
+                        data[i][l] = 0.0
         return data
     else:
         sys.stdout.write("Error: the data name is not found\n")
