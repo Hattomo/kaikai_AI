c4aafea02256367a323ab32c37169e617ce769f2
kaikai529 <kaishi.19990513@icloud.com>
2020-11-04 18:46:03 +0900

    add pooling layer

--- cnn/main.py
+++ cnn/main.py
@@ -5,9 +5,10 @@ import numpy as np
 sys.path.append('./dnn')
 sys.path.append('./dataset')
 import cnn_analysistool as catool
-import convolutional_neural_network as cl
+import convolution_layer as cl
 import csetting
 import neural_network as nn
+import pooling_layer as pl
 # import mnist
 
 # (train_data, train_label), (test_data, test_label) = mnist.load_data()
@@ -18,5 +19,7 @@ for i in range(10):
         a[i][j] = i
 
 conv = cl.Convolution_Layer(a, 3, 3)
-out = conv.convolution()
-print(out)
+conv_out = conv.convolution()
+pool = pl.Pooling_Layer(conv_out,[2,2])
+pool_out = pool.pooling()
+print(pool_out)
--- cnn/pooling_layer.py
+++ cnn/pooling_layer.py
@@ -1,11 +1,33 @@
-class Pooling_layer:
-    def pooling(data,pooling_method,pooling_size):
-        width = len(data)
-        height = len(data[0])
-        if pooling_method == "max-pooling":
-            pass
-        elif pooling_method == "mean-pooling":
-            pass
+import numpy as np
+
+class Pooling_Layer:
+
+    def __init__(self, data, pooling_size, pooling_method="max-pooling"):
+        self.data = data
+        self.pooling_method = pooling_method
+        self.pooling_size = pooling_size
+
+    def pooling(self):
+        channel = len(self.data)
+        height = len(self.data[0])
+        width = len(self.data[0][0])
+        if width % self.pooling_size[0] != 0 or height % self.pooling_size[1] != 0:
+            sys.stdout.write("Error: The pooling_size is not right\n")
+            sys.exit(1)
+        out_height = int(height / self.pooling_size[1])
+        out_width = int(width / self.pooling_size[0])
+        out = np.zeros([channel, out_width, out_width])
+        if self.pooling_method == "max-pooling":
+            for h in range(channel):
+                for i in range(out_height):
+                    for j in range(out_width):
+                        out[h][i][j] = np.max(self.data[h][i:i + self.pooling_size[1]].T[j:j + self.pooling_size[0]].T)
+        elif self.pooling_method == "mean-pooling":
+            for h in range(channel):
+                for i in range(out_height):
+                    for j in range(out_width):
+                        out[h][i][j] = np.average(self.data[h][i:i + self.pooling_size[1]].T[j:j + self.pooling_size[0]].T)
         else:
             sys.stdout.write("Error: The pooling_method is not found\n")
             sys.exit(1)
+        return out
