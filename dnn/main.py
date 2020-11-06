2f77b58d8205ba0e0be5ccbe8f5b28e99acd0be1
kaikai529 <kaishi.19990513@icloud.com>
2020-11-07 00:17:10 +0900

    refactor dnn

--- dnn/main.py
+++ dnn/main.py
@@ -7,21 +7,21 @@ import neural_network as nn
 import files
 import logic_circuit as lc
 
-layer = [795, 350, 10]
+structure = [3, 3, 1]
 epoch = 30
 logic = "or"
 # set data
-trainData = lc.dset("mnist_train", 1)
-testData = lc.dset("mnist_test", 1)
+(trainData, trainLabel) = lc.dset(logic, epoch)
+(testData, testLabel) = lc.dset(logic, 20)
 # ニューラルネットワークの生成
-orNN = nn.Neural_Network(layer)
+orNN = nn.Neural_Network(structure)
 # 学習
-count = 1
+count = 200
 # ニューラルネットワークのトレーニングデータ、レイヤー、重み番号、活性化関数番号の設定,cost func
-orNN.model(trainData, testData, "xivier", "sigmoid", "rss")
+orNN.model("xivier", "sigmoid", "rss")
 for i in range(count):
-    orNN.train()
-    orNN.test()
+    orNN.train(trainData, trainLabel)
+    orNN.test(testData, testLabel)
 atool.draw(orNN.cost)
 atool.tdchart(orNN)
 files.save(orNN)
