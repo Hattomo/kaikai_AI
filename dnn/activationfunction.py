c63d8bbeabcb0d70b6b5d18487209dfa7000ecab
kaikai529 <45989030+kaikai529@users.noreply.github.com>
2020-11-07 13:08:17 +0900

    #23 actfunc (#62)
    Co-authored-by: Hattomo <45988993+Hattomo@users.noreply.github.com>

--- dnn/activationfunction.py
+++ dnn/activationfunction.py
@@ -6,7 +6,6 @@ import numpy as np
 ## 活性化関数
 # sigmoid関数
 def non_universal_sigmoid(x):
-    # avoid overflow
     sigmoid_range = 34.538776394910684
     if x <= -sigmoid_range:
         return 1e-15
@@ -14,92 +13,99 @@ def non_universal_sigmoid(x):
         return 1.0 - 1e-15
     return 1.0 / (1.0 + np.exp(-x))
 
-# vectorize non universal sigmoid
 def sigmoid(x):
     sig = np.vectorize(non_universal_sigmoid)
     return sig(x)
 
-# msigmoid(一次元配列 x)
-def msigmoid(x):
-    ans = x
-    node = len(x)
-    if node == 1:
-        ans[0] = sigmoid(x[0])
-        return ans
-    for i in range(1, node):
-        ans[i] = sigmoid(x[i])
-        return ans
-
 # tanh関数
 def tanh(x):
-    e = np.e
-    return (e**x - e**-x) / (e**x + e**-x)
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 
-# mtanh(一次元配列　x)
-def mtanh(x):
-    ans = x
-    node = len(x)
-    for i in range(1, node):
-        ans[i] = tanh(x[i])
-        return ans
+#identity関数
+def non_universal_identity(x):
+    identity_range = 1e+5
+    if x < -identity_range:
+        return -1e+5
+    elif x > identity_range:
+        return 1e+5
+    return x
+
+def identity(x):
+    iden = np.vectorize(non_universal_identity)
+    return iden(x)
+
+def non_universal_swish(x):
+    swish_range = 1e+5
+    if x < -identity_range:
+        return -1e+5
+    elif x < identity_range:
+        return 1e+5
+    return x / (1 + np.exp(-x))
+
+def swish(x):
+    swi = np.vectorize(non_universal_swish)
+    return swi(x)
 
 # relu関数
-def relu(x):
+def non_universal_relu(x):
     if x < 0:
         return 0
     elif x > 1e+5:
         return 1e+5
-    else:
-        return x
+    return x
 
-# mrelu(一次元配列　x)
-def mrelu(x):
-    ans = x
-    node = len(x)
-    if node == 1:
-        ans[0] = relu(x[0])
-        return ans
-    for i in range(1, node):
-        ans[i] = relu(x[i])
-        return ans
+def relu(x):
+    re = np.vectorize(non_universal_relu)
+    return re(x)
+
+#ELU関数
+def non_universal_elu(x):
+    if x >= 0 and x < 1e+5:
+        return x
+    elif x > 1e+5:
+        return 1e+5
+    elif x < 0 and x > -15:
+        return np.exp(x) - 1
+    else:
+        return -1
+
+def elu(x):
+    el = np.vectorize(non_universal_elu)
+    return el(x)
 
 ## 活性化関数の微分
 # sigmoid関数の微分
 def diffsigmoid(x):
     return sigmoid(x) * (1 - sigmoid(x))
 
-# mdiffsigmoid(一次元配列 x)
-def mdiffsigmoid(x):
-    ans = x
-    node = len(x)
-    for i in range(node):
-        ans[i] = diffsigmoid(x[i])
-    return ans
-
 # tanh関数の微分
 def difftanh(x):
-    e = np.e
-    return 4 / (e**x + e**-x)**2
-
-# mdifftanh(一次元配列 x)
-def mdifftanh(x):
-    ans = x
-    node = len(x)
-    for i in range(node):
-        ans[i] = difftanh(x[i])
-    return ans
+    return 4 / (np.exp(x) + np.exp(-x))**2
 
 # relu関数の微分
-def diffrelu(x):
+def non_universal_diffrelu(x):
     if x < 0:
         return 0
     else:
         return 1
 
-# mdiffrelu(一次元配列 x)
-def mdiffrelu(x):
-    ans = x
-    node = len(x)
-    for i in range(node):
-        ans[i] = diffrelu(x[i])
-    return ans
+# diffrelu
+def diffrelu(x):
+    drelu = np.vectorize(non_universal_diffrelu)
+    return drelu(x)
+
+# identityの微分
+def diffidentity(x):
+    return 1
+
+def non_universal_diffelu(x):
+    if x >= 0:
+        return 1
+    elif x < 0 and x > -15:
+        return np.exp(x)
+    else:
+        return 0
+
+def diffelu(x):
+    delu = np.vectorize(non_universal_diffelu)
+    return delu(x)
