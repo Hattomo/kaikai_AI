1162576a99a03e275a45c41edb391fa82e3c28a0
Hattomo (TomohiroHattori) <45988993+Hattomo@users.noreply.github.com>
2020-11-08 19:28:54 +0900

    Avoid tdchart error, due to dimention is wrong (#78)

--- dnn/analysistool.py
+++ dnn/analysistool.py
@@ -14,21 +14,24 @@ def draw(y):
     #plt.show()
 
 def tdchart(nn):
-    fig = plt.figure()
-    ax = Axes3D(fig)
-    ax.set_xlabel('x0')
-    ax.set_ylabel('x1')
-    ax.set_zlabel('y')
-    x = []
-    y = []
-    z = []
-    dence = 30
-    for i in range(dence):
-        for j in range(dence):
-            x.append(i / dence)
-            y.append(j / dence)
-            nn.forwordpropagation([i / dence, j / dence])
-            z.append(nn.z[-1])
-    ax.scatter3D(x, y, z)
-    plt.savefig(os.path.join(os.path.dirname(__file__), '../out/3d.png'))
-    #plt.show()
+    if (nn.structure[-1] != 1):
+        print("tdchart : dimention is not right")
+    else:
+        fig = plt.figure()
+        ax = Axes3D(fig)
+        ax.set_xlabel('x0')
+        ax.set_ylabel('x1')
+        ax.set_zlabel('y')
+        x = []
+        y = []
+        z = []
+        dence = 30
+        for i in range(dence):
+            for j in range(dence):
+                x.append(i / dence)
+                y.append(j / dence)
+                nn.forwordpropagation([i / dence, j / dence])
+                z.append(nn.z[-1])
+        ax.scatter3D(x, y, z)
+        plt.savefig(os.path.join(os.path.dirname(__file__), '../out/3d.png'))
+        #plt.show()
