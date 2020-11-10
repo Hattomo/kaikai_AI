09a1e358fc57899fb6ca7a58664de3c6d855f54a
kaikai529 <kaishi.19990513@icloud.com>
2020-11-10 23:47:43 +0900

    clean_pl

--- cnn/pooling_layer.py
+++ cnn/pooling_layer.py
@@ -6,12 +6,12 @@ class Pooling_Layer:
 
     def __init__(self, pooling_size, pooling_method="max-pooling"):
         self.poolfunc = self.__set_pooling_method(pooling_method)
-        self.pooling_size = pooling_size
+        self.p_size = pooling_size
         self.index = []
 
     def __get_index(self, partical_data):
         max_ = np.argmax(partical_data)
-        return (max_ // self.pooling_size[0], max_ % self.pooling_size[1])
+        return (max_ // self.p_size[0], max_ % self.p_size[1])
 
     def __set_pooling_method(self, pooling_method):
         if pooling_method == "max-pooling":
@@ -23,33 +23,31 @@ class Pooling_Layer:
 
     def forwordpropagation(self, train_data):
         (data_channel, data_height, data_width) = np.shape(train_data)
-        (pooling_height, pooling_width) = np.shape(self.pooling_size)
         # check pooling size
-        if data_height % pooling_height != 0 or width % pooling_width != 0:
+        if data_height % self.p_size[0] != 0 or data_width % self.p_size[1] != 0:
             sys.stdout.write("Error: The pooling_size is not right\n")
             sys.exit(1)
         # pooling
-        p_result_height = data_height // pooling_height
-        p_result_width = data_width // pooling_width
+        p_result_height = data_height // self.p_size[0]
+        p_result_width = data_width // self.p_size[1]
         p_result = np.zeros([data_channel, p_result_height, p_result_width])
         for h in range(data_channel):
             for i in range(p_result_height):
                 for j in range(p_result_width):
-                    partical_data = train_data[h][i * pooling_height:(i+1) * pooling_height,
-                                                  j * pooling_width:(j+1) * pooling_width]
+                    partical_data = train_data[h][i * self.p_size[0]:(i+1) * self.p_size[0],
+                                                  j * self.p_size[1]:(j+1) * self.p_size[1]]
                     p_result[h][i][j] = self.poolfunc(partical_data)
                     self.index.append(self.__get_index(partical_data))
         return p_result
 
     def backpropagation(self, input_error):
         (input_channel, input_height, input_width) = np.shape(input_error)
-        (pooling_height, pooling_width) = np.shape(self.pooling_size)
-        output_error = np.zeros([input_channel, input_height * pooling_height, width * pooling_width])
+        output_error = np.zeros([input_channel, input_height * self.p_size[0], width * self.p_size[1]])
         count = 0
         for h in range(input_channel):
-            for i in range(input_height // pooling_height):
-                for j in range(input_width // pooling_width):
-                    output_error[h][i*pooling_height + self.index[count][0],
-                                    j*pooling_width + self.index[count][1]] = input_error[h][i][j]
+            for i in range(input_height // self.p_size[0]):
+                for j in range(input_width // self.p_size[1]):
+                    output_error[h][i*self.p_size[0] + self.index[count][0],
+                                    j*self.p_size[1] + self.index[count][1]] = input_error[h][i][j]
                     count += 1
         return output_error
