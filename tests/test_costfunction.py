34945445f3b6e74b6f87bba7dac197a73c854154
kaikai529 <kaishi.19990513@icloud.com>
2020-11-11 01:00:43 +0900

    make test

---
+++ tests/test_costfunction.py
@@ -0,0 +1,26 @@
+import sys
+import unittest
+
+import numpy as np
+
+sys.path.append('./shared')
+import activationfunction as af
+
+class Test_Cost_Function(unittest.TestCase):
+
+    def setUp(self):
+        # init
+        pass
+
+    def tearDown(self):
+        # dispose
+        pass
+
+    def test_rss(self):
+        pass
+    
+    def test_cross_entropy(self):
+        pass 
+
+if __name__ == "__main__":
+    unittest.main()
