fd317b71aa494dbe9be0fba2837372a60ac1122d
kaikai529 <kaishi.19990513@icloud.com>
2020-11-10 23:53:02 +0900

    test pooling fp

---
+++ tests/test_pooling_layer.py
@@ -0,0 +1,20 @@
+import sys
+
+import numpy as np
+import pytest
+
+sys.path.append('./cnn')
+sys.path.append("./shared")
+import convolution_layer as cl
+
+def test_pooling_forwordpropagation():
+    a = np.zeros([1, 4, 4])
+    count = 0
+    for i in range(4):
+        for j in range(4):
+            a[0][i][j] = count
+            count += 1
+    pool = pl.Pooling_Layer([2, 2])
+    out = pool.forwordpropagation(a)
+    ans = np.array([[[5., 7.], [13., 15.]]])
+    assert ((out - ans) < 1e-3).all()
