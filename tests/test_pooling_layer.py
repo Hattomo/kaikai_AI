25764558de72d1393a97180cb557e40f67ff25dc
kaikai529 <kaishi.19990513@icloud.com>
2020-11-11 00:19:05 +0900

    modify

--- tests/test_pooling_layer.py
+++ tests/test_pooling_layer.py
@@ -5,7 +5,7 @@ import pytest
 
 sys.path.append('./cnn')
 sys.path.append("./shared")
-import convolution_layer as cl
+import pooling_layer as pl
 
 def test_pooling_forwordpropagation():
     a = np.zeros([1, 4, 4])
