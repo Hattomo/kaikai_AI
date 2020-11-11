d827af1bb24325676d50f5b953c3b36fc4c37881
Hattomo (TomohiroHattori) <45988993+Hattomo@users.noreply.github.com>
2020-11-11 15:47:26 +0900

    Adopt 3D graph for N dimension (#110)

--- dnn/analysistool.py
+++ dnn/analysistool.py
@@ -20,7 +20,7 @@ def draw(y):
     #plt.show()
 
 def tdchart(nn):
-    if (nn.structure[-1] != 1):
+    if (nn.structure[-1] == 1):
         print("tdchart : dimention is not right")
     else:
         fig = plt.figure()
@@ -28,16 +28,18 @@ def tdchart(nn):
         ax.set_xlabel('x0')
         ax.set_ylabel('x1')
         ax.set_zlabel('y')
-        x = []
-        y = []
-        z = []
         dence = 30
-        for i in range(dence):
-            for j in range(dence):
-                x.append(i / dence)
-                y.append(j / dence)
-                nn.forwordpropagation([i / dence, j / dence])
-                z.append(nn.z[-1])
-        ax.scatter3D(x, y, z, c="darkorange")
+        for h in range(len(nn.z[-1])):
+            x = []
+            y = []
+            z = []
+            for i in range(dence):
+                for j in range(dence):
+                    x.append(i / dence)
+                    y.append(j / dence)
+                    nn.forwordpropagation([i / dence, j / dence])
+                    z.append(nn.z[-1][h])
+            ax.scatter3D(x, y, z, label=h)
+            ax.legend()
         plt.savefig(os.path.join(os.path.dirname(__file__), '../out/3d.png'))
         #plt.show()
