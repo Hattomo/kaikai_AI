2712c11ab49f32927262d2230936230c358e581e
kaikai529 <45989030+kaikai529@users.noreply.github.com>
2020-11-14 20:25:40 +0900

    modify entropy (#125)
    * modify entropy
    
    * format
    
    * add test
    
    * refactor entropy
    
    * divide test
    
    * modify

--- tests/test_costfunction.py
+++ tests/test_costfunction.py
@@ -1,26 +1,26 @@
 import sys
-import unittest
 
 import numpy as np
 
 sys.path.append('./shared')
-import activationfunction as af
+import costfunction
 
-class Test_Cost_Function(unittest.TestCase):
+def test_rss():
+    pass
 
-    def setUp(self):
-        # init
-        pass
+def test_cross_entropy():
+    label = np.array([0, 0, 1, 1])
+    ans = np.array([0.2, 0.3, 0.4, 0.5])
+    error = costfunction.cross_entropy(label, ans)
+    print(error)
+    assert np.isclose(error, 2.189, rtol=1e-3, atol=1e-3), "check cross_entropy or test"
 
-    def tearDown(self):
-        # dispose
-        pass
+def test_diffcross_entropy():
+    label = np.array([0, 0, 1, 1])
+    ans = np.array([0.2, 0.3, 0.4, 0.5])
+    error = costfunction.diffcross_entropy(label, ans)
+    assert np.isclose(error, np.array([1.250, 1.428, -2.500, -2.000]), rtol=1e-3,
+                      atol=1e-3).all(), "check diffcross_entropy or test"
 
-    def test_rss(self):
-        pass
-
-    def test_cross_entropy(self):
-        pass
-
-if __name__ == "__main__":
-    unittest.main()
+test_cross_entropy()
+test_diffcross_entropy()
