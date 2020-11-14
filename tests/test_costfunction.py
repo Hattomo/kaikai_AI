bbd9902123f2d745159e3949d73efc179ff67e87
kaikai529 <kaishi.19990513@icloud.com>
2020-11-14 20:00:15 +0900

    divide test

--- tests/test_costfunction.py
+++ tests/test_costfunction.py
@@ -5,14 +5,6 @@ import numpy as np
 sys.path.append('./shared')
 import costfunction
 
-def setUp():
-    # init
-    pass
-
-def tearDown():
-    # dispose
-    pass
-
 def test_rss():
     pass
 
@@ -20,6 +12,15 @@ def test_cross_entropy():
     label = np.array([0, 0, 1, 1])
     ans = np.array([0.2, 0.3, 0.4, 0.5])
     error = costfunction.cross_entropy(label, ans)
-    assert error - 2.189 < 1e-3, "check cross_entropy or test"
+    print(error)
+    assert np.isclose(error, 2.189, rtol=1e-3, atol=1e-3), "check cross_entropy or test"
+
+def test_diffcross_entropy():
+    label = np.array([0, 0, 1, 1])
+    ans = np.array([0.2, 0.3, 0.4, 0.5])
     error = costfunction.diffcross_entropy(label, ans)
-    assert (error - [1.25, 1.428, -2.50, -2.00]).all() < 1e-3, "check diffcross_entropy or test"
+    assert np.isclose(error, np.array([1.250, 1.428, -2.500, -2.000]), rtol=1e-3,
+                      atol=1e-3).all(), "check diffcross_entropy or test"
+
+test_cross_entropy()
+test_diffcross_entropy()
