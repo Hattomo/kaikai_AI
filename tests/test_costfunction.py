1c72e183760581fc23602c552a38a8e1690f4ef2
kaikai529 <kaishi.19990513@icloud.com>
2020-11-14 20:58:59 +0900

    add test

--- tests/test_costfunction.py
+++ tests/test_costfunction.py
@@ -6,13 +6,21 @@ sys.path.append('./shared')
 import costfunction
 
 def test_rss():
-    pass
+    label = np.array([0, 0, 1, 1])
+    ans = np.array([0.2, 0.3, 0.4, 0.5])
+    error = costfunction.rss(label, ans)
+    assert np.isclose(error, 0.74), "check rss or test"
+
+def test_diffrss():
+    label = np.array([0, 0, 1, 1])
+    ans = np.array([0.2, 0.3, 0.4, 0.5])
+    error = costfunction.diffrss(label, ans)
+    assert np.isclose(error, np.array([0.2, 0.3, -0.6, -0.5])).all(), "check rss or test"
 
 def test_cross_entropy():
     label = np.array([0, 0, 1, 1])
     ans = np.array([0.2, 0.3, 0.4, 0.5])
     error = costfunction.cross_entropy(label, ans)
-    print(error)
     assert np.isclose(error, 2.189, rtol=1e-3, atol=1e-3), "check cross_entropy or test"
 
 def test_diffcross_entropy():
@@ -22,5 +30,4 @@ def test_diffcross_entropy():
     assert np.isclose(error, np.array([1.250, 1.428, -2.500, -2.000]), rtol=1e-3,
                       atol=1e-3).all(), "check diffcross_entropy or test"
 
-test_cross_entropy()
-test_diffcross_entropy()
+test_diffrss()
