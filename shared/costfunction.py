6960e7fd94d9f8308a6ab848666ab854eaaaca22
kaikai529 <kaishi.19990513@icloud.com>
2020-11-14 12:20:54 +0900

    refactor entropy

--- shared/costfunction.py
+++ shared/costfunction.py
@@ -14,20 +14,12 @@ def rss(label, ans):
 def diffrss(label, ans):
     return ans - label
 
-def non_universal_cross_entropy(label, ans):
-    if ans < 0:
+def cross_entropy(label, ans):
+    if ans.any() < 0:
         sys.stdout.write(
             "Calculation Error(cross entropy): The actfunc is not right.\nplease change actfunc of return only plus\n")
         sys.exit(1)
-    return -label * math.log(ans) - (1-label) * math.log(1 - ans)
-
-def cross_entropy(label, ans):
-    centropy = np.vectorize(non_universal_cross_entropy)
-    return np.sum(centropy(label, ans))
-
-def non_universal_diffcross_entropy(label, ans):
-    return -label / (ans) + (1-label) / (1-ans)
+    return np.sum(-label * np.log(ans) - (1-label) * np.log(1 - ans))
 
 def diffcross_entropy(label, ans):
-    diffcentropy = np.vectorize(non_universal_diffcross_entropy)
-    return diffcentropy(label, ans)
+    return -label / (ans) + (1-label) / (1-ans)
