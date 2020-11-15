b699f90ae4af671152466626380c7cac4c80aec3
kaikai529 <kaishi.19990513@icloud.com>
2020-11-15 21:55:02 +0900

    change test

--- tests/test_activationfunction.py
+++ tests/test_activationfunction.py
@@ -1,80 +1,66 @@
 import sys
-import unittest
 
 import numpy as np
 
 sys.path.append('./shared')
 import activationfunction as af
 
-class Test_Activation_Function(unittest.TestCase):
+def test_sigmoid():
+    data = np.array([-50, 50, 0])
+    label = np.array([1e-15, 1.0 - 1e-15, 0.5])
+    assert np.allclose(af.sigmoid(data), label), "Sigmoid is test error"
 
-    def setUp(self):
-        # init
-        pass
+def test_relu():
+    data = np.array([-50, 50, 0])
+    label = np.array([0, 50, 0])
+    assert np.allclose(af.relu(data), label), "ReLU is test error"
 
-    def tearDown(self):
-        # dispose
-        pass
+def test_tanh():
+    data = np.array([-50, 50, 0])
+    label = np.array([-1, 1, 0])
+    assert np.allclose(af.tanh(data), label), "tanh is test error"
 
-    def test_sigmoid(self):
-        self.assertEqual(af.sigmoid(-50), np.array(1e-15))
-        self.assertEqual(af.sigmoid(50), np.array(1.0 - 1e-15))
-        self.assertEqual(af.sigmoid(0), np.array(0.5))
+def test_identity():
+    data = np.array([-50, 50, 0])
+    label = np.array([-50, 50, 0])
+    assert np.allclose(af.identity(data), label), "Identity is test error"
 
-    def test_relu(self):
-        self.assertEqual(af.relu(-50), np.array(0))
-        self.assertEqual(af.relu(50), np.array(50))
-        self.assertEqual(af.relu(0), np.array(0))
+def test_swish():
+    data = np.array([-1, 0, 1])
+    label = np.array([-0.2689414213699951, 0, 0.7310585786300049])
+    assert np.allclose(af.swish(data), label), "Swish is test error"
 
-    def test_tanh(self):
-        self.assertEqual(af.tanh(0), np.array(0))
-        self.assertEqual(af.tanh(50), np.array(1))
-        self.assertEqual(af.tanh(-50), np.array(-1))
+def test_elu():
+    data = np.array([0, 4, -50])
+    label = np.array([0, 4, -1])
+    assert np.allclose(af.elu(data), label), "ELU is test error"
 
-    def test_identity(self):
-        self.assertEqual(af.identity(50), np.array(50))
-        self.assertEqual(af.identity(-50), np.array(-50))
-        self.assertEqual(af.identity(0), np.array(0))
-        self.assertEqual(af.identity(10e+5), np.array(1e5))
+def test_diffsigmoid():
+    data = np.array([0, 1])
+    label = np.array([0.25, 0.19661193324148185])
+    assert np.allclose(af.diffsigmoid(data), label), "diffsigmoid is test error"
 
-    def test_swish(self):
-        self.assertEqual(af.swish(0), np.array(0))
-        self.assertEqual(af.swish(1e+15), np.array(1e5))
+def test_diffrelu():
+    data = np.array([-50, 50, 0])
+    label = np.array([0, 1, 1])
+    assert np.allclose(af.diffrelu(data), label), "diffrelu is test error"
 
-    def test_elu(self):
-        self.assertEqual(af.elu(0), np.array(0))
-        self.assertEqual(af.elu(4), np.array(4))
-        self.assertEqual(af.elu(-50), np.array(-1))
+def test_difftanh():
+    data = np.array([-0.3, 0, 0.9])
+    label = np.array([0.9151369618266293, 1, 0.48691736114834144])
+    assert np.allclose(af.difftanh(data), label), "difftanh is test error"
 
-    def test_diffsigmoid(self):
-        self.assertEqual(af.diffsigmoid(0), np.array(0.25))
-        self.assertTrue(np.isclose(af.diffsigmoid(1), np.array(0.19661193324148185)))
+def test_diffidentity():
+    data = np.array([-50, 50, 0])
+    label = np.array([1, 1, 1])
+    assert np.allclose(af.diffidentity(data), label), "diffidentity is test error"
 
-    def test_diffrelu(self):
-        self.assertEqual(af.diffrelu(-50), np.array(0))
-        self.assertEqual(af.diffrelu(0), np.array(1))
-        self.assertEqual(af.diffrelu(50), np.array(1))
+def test_diffswish():
+    data = np.array([-4, 0, 4])
+    label = np.array([-0.05266461, 0.5, 1.05266461])
+    assert np.allclose(af.diffswish(data), label), "diffswish is test error"
 
-    def test_difftanh(self):
-        self.assertEqual(af.difftanh(0), np.array(1))
-        self.assertTrue(af.difftanh(4), np.array(0))
-        self.assertTrue(af.difftanh(4), np.array(0))
-
-    def test_diffidentity(self):
-        self.assertEqual(af.diffidentity(50), np.array(1))
-        self.assertEqual(af.diffidentity(-50), np.array(1))
-        self.assertEqual(af.diffidentity(0), np.array(1))
-        self.assertEqual(af.diffidentity(10e+5), np.array(1))
-
-    def test_diffswish(self):
-        self.assertEqual(af.diffswish(0), np.array(0.5))
-        self.assertTrue(af.diffswish(-4), np.array(-0.05266461))
-        self.assertTrue(af.diffswish(4), np.array(1.05266461))
-
-    def test_diffelu(self):
-        self.assertEqual(af.diffelu(0), np.array(1))
-        self.assertEqual(af.diffelu(4), np.array(1))
-        self.assertTrue(np.isclose(af.diffelu(-1), np.array(0.36787944)))
-
-if __name__ == "__main__":
-    unittest.main()
+def test_diffelu():
+    data = np.array([-1, 0, 4])
+    label = np.array([0.36787944, 1, 1])
+    assert np.allclose(af.diffelu(data), label), "diffelu is test error"
