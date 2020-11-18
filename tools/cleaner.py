6f64cb6ef0c3a4e75b1e14b87776c98130302110
Hattomo <45988993+Hattomo@users.noreply.github.com>
2020-11-18 14:54:48 +0900

    aa

--- tools/cleaner.py
+++ tools/cleaner.py
@@ -6,13 +6,13 @@ import re
 now = time.time()
 path = os.path.join(os.path.dirname(__file__), '../out/**/*')
 # a = glob.glob(path, recursive=True)
-a = [p for p in glob.glob(path, recursive=True) if (os.path.isfile(p) and re.search("20\d{18}.*", p))]
-print(a)
-for file_name in a:
+files = [p for p in glob.glob(path, recursive=True) if (os.path.isfile(p) and re.search("20\d{18}(|_cost|_accurancy).*", p))]
+print(files)
+for file_name in files:
     # one week passed since created
     if 604800 < now - os.path.getctime(file_name):
         #os.remove(file_name)
         print("delete " + str(file_name))
-# new_path = "demo_folder"  #フォルダ名
-# if not os.path.exists(new_path):  #ディレクトリがなかったら
-#     os.mkdir(new_path)  #作成したいフォルダ名を作成
+# new_path = "demo_folder" 
+# if not os.path.exists(new_path):
+#     os.mkdir(new_path)
