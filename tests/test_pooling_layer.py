a9c33d61040be4ae8d3e6e2b562f7027417bcdac
kaikai529 <kaishi.19990513@icloud.com>
2020-11-20 12:00:24 +0900

    delite pool test

--- tests/test_pooling_layer.py
+++ tests/test_pooling_layer.py
@@ -7,14 +7,4 @@ sys.path.append('./cnn')
 sys.path.append("./shared")
 import pooling_layer as pl
 
-def test_pooling_forwordpropagation():
-    a = np.zeros([1, 4, 4])
-    count = 0
-    for i in range(4):
-        for j in range(4):
-            a[0][i][j] = count
-            count += 1
-    pool = pl.Pooling_Layer([2, 2])
-    out = pool.forwordpropagation(a)
-    ans = np.array([[[5., 7.], [13., 15.]]])
-    assert ((out - ans) < 1e-3).all()
+
