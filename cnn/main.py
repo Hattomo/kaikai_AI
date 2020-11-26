196695bb6dce4a6153dce7a87d8bb0e19d982adf
kaikai529 <kaishi.19990513@icloud.com>
2020-11-26 23:41:46 +0900

    fix

--- cnn/main.py
+++ cnn/main.py
@@ -15,9 +15,9 @@ import mnist
 import logic_circuit as lc
 
 batch = 4
-epoch = 1000
+epoch = 100
 
-Data, Label = lc.dset("cnn_ex", epoch * batch // 4)
+Data, Label = lc.dset("cnn_exs", epoch * batch // 2)
 
 data_num, D_channel, D_height, D_width = np.shape(Data)
 data_num, L_num = np.shape(Label)
@@ -26,12 +26,12 @@ trainLabel = Label.reshape([epoch, batch, -1])
 
 conv = cl.Convolution_Layer(in_channel=1, out_channel=8, ksize=3, pad=1)
 pool = pl.Pooling_Layer(pooling_size=[2, 2])
-fullc = fc.Fully_Connect_Layer([32 + 1, 10, 4], batch)
+fullc = fc.Fully_Connect_Layer([32 + 1, 10, 2], batch)
 
 for i in range(epoch):
     conv_out = conv.forwardpropagation(trainData[i])
     pool_out = pool.forwardpropagation(conv_out)
     error = fullc.train(pool_out, trainLabel[i])
-    # pool_error = pool.backpropagation(error)
-    # conv.backpropagation(pool_error)
+    pool_error = pool.backpropagation(error)
+    conv.backpropagation(pool_error)
     fullc.test(pool_out, trainLabel[i])
