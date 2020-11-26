7971169a3b2c134180c1a3842533a3bcc616b9b3
kaikai529 <kaishi.19990513@icloud.com>
2020-11-26 12:05:14 +0900

    fix bug

--- shared/numpy_files.py
+++ shared/numpy_files.py
@@ -8,7 +8,8 @@ def save(nn):
     np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
     path = os.path.join(os.path.dirname(__file__), '../out/np_savez')
     # input data is only numpy
-    np.savez(path, weight=nn.weight[0])
+    for i in range(len(nn.weight))
+        np.savez(path, weight=nn.weight[i])
     np.warnings.filterwarnings('default', category=np.VisibleDeprecationWarning)
 
 # load weight
