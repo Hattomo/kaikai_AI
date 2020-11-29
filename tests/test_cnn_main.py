80013004053ab21efed4a8bad0280fe6893e4f09
Hattomo <45988993+Hattomo@users.noreply.github.com>
2020-11-29 17:11:06 +0900

    delete main tests

--- tests/test_cnn_main.py
+++ tests/test_cnn_main.py
@@ -12,5 +12,4 @@ import neural_network as nn
 import logic_circuit as lc
 
 def test_main():
-    main_result = subprocess.run(('python', 'cnn/main.py'))
-    assert main_result.returncode == 0
+    pass
