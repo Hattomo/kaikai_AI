bf03eeaadb0ecffdd20b597316627fffd956fefb
kaikai529 <kaishi.19990513@icloud.com>
2020-11-30 20:30:36 +0900

    format

--- cnn/csetting.py
+++ cnn/csetting.py
@@ -12,6 +12,7 @@ def select_kernel(k_method, in_channel, out_channel, ksize):
         return cnn_unif(in_channel, out_channel, ksize)
     sys.stdout.write("Error: The kernel method is not found\n")
     sys.exit(1)
+
 #一様分布
 def cnn_unif(in_channel, out_channel, ksize):
     kernel = np.random.rand(in_channel * out_channel * ksize**2) * 2 - 1
@@ -27,4 +28,3 @@ def cnn_xavier(in_channel, out_channel, ksize):
 def cnn_he(in_channel, out_channel, ksize):
     kernel = np.random.normal(loc=0.0, scale=1 / math.sqrt(2 / out_channel), size=in_channel * out_channel * ksize**2)
     return kernel.reshape(out_channel, in_channel, ksize, ksize)
-
