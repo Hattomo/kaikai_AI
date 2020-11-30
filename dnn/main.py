7d0bd5948eaecdf2b657cf7d7b32cae3dbddfc03
kaikai529 <kaishi.19990513@icloud.com>
2020-11-30 13:21:51 +0900

    fix

--- dnn/main.py
+++ dnn/main.py
@@ -5,6 +5,7 @@ import numpy as np
 sys.path.append("./dataset")
 sys.path.append("./shared")
 import activationfunction as af
+import analysistool as atool
 import neural_network as nn
 import dataset
 
@@ -16,9 +17,11 @@ trainData, trainLabel = dataset.logic(logic,datasize,batch)
 testData, testLabel = dataset.logictest(logic,10)
 # ニューラルネットワークの生成
 structure = [2 + 1, 5, 2]
-myNN = nn.Neural_Network(structure,actfunc="tanh")
+myNN = nn.Neural_Network(structure)
 # # 学習
 epoch = 200
 for i in range(epoch):
     myNN.train(trainData, trainLabel)
     myNN.test(testData, testLabel)
+
+atool.draw(myNN.cost)
