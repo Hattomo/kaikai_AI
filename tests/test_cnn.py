397e42ab4b19f0436be97da3afb914d188552036
kaikai529 <kaishi.19990513@icloud.com>
2020-11-30 21:23:17 +0900

    merge test

--- tests/test_cnn.py
+++ tests/test_cnn.py
@@ -6,6 +6,12 @@ import numpy as np
 sys.path.append("./dataset")
 sys.path.append('./dnn')
 sys.path.append("./shared")
+import convolution_layer as cl
 
 def test_main():
+    pass
+
+
+
+def test_concolution_layer_forwardpropagation():
     pass
--- tests/test_dnn.py
+++ tests/test_dnn.py
@@ -5,10 +5,7 @@ import numpy as np
 sys.path.append("./dataset")
 sys.path.append('./dnn')
 sys.path.append("./shared")
-import activationfunction as af
-import analysistool as atool
 import neural_network as nn
-import numpy_files as npfiles
 
 def test_main_or_with_dropout():
     datasetsize = 8
@@ -42,3 +39,23 @@ def test_main_or_without_dropout():
     # 学習
     for i in range(epoch):
         myNN.train(trainData, trainLabel)
+
+def test_forwordpropagation():
+    pass
+
+def test_backpropagation():
+    pass
+
+def test_dropout_shake():
+    structure = [3, 4, 2]
+    dropout = [0, 0.9, 0]
+    dnn = nn.Neural_Network(structure, dropout)
+    dnn.do[0] = np.array([[0, 0, 1], [0, 1, 1], [1, 0, 1]])
+    dnn._Neural_Network__dropout_shake(False)
+    assert np.all(dnn.do[0] == np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
+    assert np.all(dnn.do[1] == np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1],
+    ]))
