1e2ec97cedba4223eaa150971eb4e0f5a030f96b
Hattomo <45988993+Hattomo@users.noreply.github.com>
2020-12-06 21:24:22 +0900

    fix test

--- tests/test_analysistool.py
+++ tests/test_analysistool.py
@@ -2,7 +2,7 @@ import sys
 
 import numpy as np
 
-sys.path.append('./dnn')
+sys.path.append('./shared')
 import analysistool as atool
 
 def test_draw():
@@ -12,3 +12,7 @@ def test_draw():
 def test_accurancygraph():
     y = [0, 0.2, 0.6, 0.7, 0.1, 0.9, 0.7, 0.3]
     atool.accurancygraph(y, "test")
+
+def test_kernel_move():
+    y = [[9, 0, 8, 1, 7, 2, 6, 3, 5, 4], [9, 0, 8, 1, 7, 2, 6, 3, 5, 4], [9, 0, 8, 1, 7, 2, 6, 3, 5, 4]]
+    atool.kernelmove(y, "test")
