a95cbed84ddf9d61d5a515047ff3583205fb9d81
kaikai529 <kaishi.19990513@icloud.com>
2020-12-15 23:32:38 +0900

    format

--- rnn/main.py
+++ rnn/main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import reccurent_neural_network as rnn
 
-structure = [3,3,2]
+structure = [3, 3, 2]
 myrnn = rnn.Reccurent_Neural_Network(structure)
 train_data = np.ones(2)
 error = np.ones(2)
--- rnn/reccurent_neural_network.py
+++ rnn/reccurent_neural_network.py
@@ -18,7 +18,7 @@ class Reccurent_Neural_Network():
         self.costfunc, self.diffcost = cf.set_costfunc(costfunc)
         self.layerstocker = list()
         self.errorstocker = list()
-    
+
     # set all weight
     def set_weight(self, structure):
         weight_num = len(structure)
@@ -32,14 +32,14 @@ class Reccurent_Neural_Network():
     def set_reccurent_weight(self, structure):
         weight_num = len(structure)
         weight_layer = list()
-        for i in range(weight_num-2):
-            weight_layer.append(np.ones([structure[i+1]-1,structure[i+1]]))
+        for i in range(weight_num - 2):
+            weight_layer.append(np.ones([structure[i + 1] - 1, structure[i + 1]]))
         return weight_layer
 
     # override
-    def forwardpropagation(self,train_data):
+    def forwardpropagation(self, train_data):
         # ひとつ前のデータ
-        self.z_1,self.y_1 = self.set_layer(self.structure)
+        self.z_1, self.y_1 = self.set_layer(self.structure)
         # 今回のデータ
         self.z, self.y = self.set_layer(self.structure)
         # input layer
@@ -47,7 +47,7 @@ class Reccurent_Neural_Network():
         # middle layer
         for i in range(len(self.structure) - 2):
             self.y[i + 1][1:] = self.z[i] @ self.weight[i].T + self.z_1[i] @ self.reccurent_weight[i].T
-            self.z[i + 1] = self.actfunc(self.y[i + 1]) 
+            self.z[i + 1] = self.actfunc(self.y[i + 1])
         # output layer
         self.y[-1] = self.z[-2] @ self.weight[-1].T
         self.z[-1] = self.actfunc(self.y[-1])
@@ -61,8 +61,8 @@ class Reccurent_Neural_Network():
             znet.append(x)
             ynet.append(y)
         return znet, ynet
-    
-    def backpropagation(self,train_label):
+
+    def backpropagation(self, train_label):
         train_ratio = 0.1
         # ひとつ前のエラーを格納
         self.error_1 = self.set_error(self.structure)
@@ -70,11 +70,12 @@ class Reccurent_Neural_Network():
         self.error = self.set_error(self.structure)
         # バックプロパゲーション
         self.error[-1] = self.diffact(self.y[-1]) * self.diffcost(train_label, self.z[-1])
-        self.weight[-1] -= train_ratio *  self.vvmat(self.z[-2],self.error[-1])
+        self.weight[-1] -= train_ratio * self.vvmat(self.z[-2], self.error[-1])
         # middle layer to input layer
         for i in range(len(self.structure) - 2):
-            self.error[-i] = self.diffact(self.z[-i - 2][1:]) * (self.weight[-i - 1][:,1:] @ self.error[-i+1][1:])
-            self.weight[-i - 2] -= train_ratio * self.vvmat(self.z[-i - 3],self.error_1[-i+1][1:] + self.error[-i+1][1:])
+            self.error[-i] = self.diffact(self.z[-i - 2][1:]) * (self.weight[-i - 1][:, 1:] @ self.error[-i + 1][1:])
+            self.weight[-i - 2] -= train_ratio * self.vvmat(self.z[-i - 3],
+                                                            self.error_1[-i + 1][1:] + self.error[-i + 1][1:])
 
     # エラーを格納する
     def set_error(self, structure):
@@ -83,8 +84,8 @@ class Reccurent_Neural_Network():
             error.append(np.zeros(structure[i]))
         return error
 
-    def vvmat(self,a,b):
-        mat = np.zeros([len(b),len(a)])
+    def vvmat(self, a, b):
+        mat = np.zeros([len(b), len(a)])
         for i in range(len(b)):
             for j in range(len(a)):
                 mat[i][j] = b[i] * a[j]
