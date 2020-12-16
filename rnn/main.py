e3c4be3e0a1ec9b8397fc9fc4bf469d0ee79af09
kaikai529 <45989030+kaikai529@users.noreply.github.com>
2020-12-16 23:35:17 +0900

    <提案>RNNの基本的な構造 (#201)
    * make rnn
    
    * format

---
+++ rnn/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+import reccurent_neural_network as rnn
+
+structure = [3, 3, 2]
+myrnn = rnn.Reccurent_Neural_Network(structure)
+train_data = np.ones(2)
+error = np.ones(2)
+myrnn.forwardpropagation(train_data)
+myrnn.backpropagation(error)
+print()
